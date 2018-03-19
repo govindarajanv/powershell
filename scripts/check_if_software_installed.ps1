613791c75805bc193606901adc2de7a5c487b176
Govindarajan Vishnuchithan <govindarajanv@gmail.com>
2018-03-19 16:46:05 +0530

    added second way to check if a software is installed or not

--- scripts/check_if_software_installed.ps1
+++ scripts/check_if_software_installed.ps1
@@ -1,3 +1,6 @@
 # The script check if Textpad is successfully installed or not
 Get-ItemProperty 'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*' | .{process{if($_.DisplayName -and $_.UninstallString) { $_ } }}
 | Select DisplayName, Publisher, InstallDate, DisplayVersion, UninstallString |Sort DisplayName | where {$_.DisplayName -like "*Textpad*" }
+
+#another way to check
+if ((Get-WmiObject -Class Win32_Product | Where-Object { $_.Name -eq "WebEx Productivity Tools"}).Version -eq "2.82.7000.1167") { Return $True } Return $False
